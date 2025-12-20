8c9362991afbcf29d7d783d65f19cf51498cbad0
Jesper Corba <37336169+MrCorba@users.noreply.github.com>
2025-12-20 21:15:49 +0100

    Add page title

--- FocusPointClient/src/pages/router.ts
+++ FocusPointClient/src/pages/router.ts
@@ -4,11 +4,20 @@ import HomeView from './HomeView.vue'
 import SettingsView from './SettingsView.vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/settings', component: SettingsView },
+  { path: '/', component: HomeView, meta: { title: 'Home' }  },
+  { path: '/settings', component: SettingsView , meta: { title: 'Settings' } },
 ]
 
-export const router = createRouter({
+const router = createRouter({
   history: createMemoryHistory(),
   routes,
+});
+
+router.afterEach((to) => {
+  const APP_TITLE = 'FocusPoint'
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+  const title = nearestWithTitle ? `${nearestWithTitle.meta.title} | ` : ''
+  document.title = `${title}${APP_TITLE}`
 })
+
+export { router };
